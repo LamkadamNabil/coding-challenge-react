fd4a149e43cb2ce66df9d682fb60e78d28e5bf4f
LamkadamNabil <na.lamkadam@gmail.com>
2023-10-22 20:46:16 +0100

    challenge N1 for color render

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ColorRenderer from "./component/ColorRenderer";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>
+        Use the Color component to render each item in the colors array on the
+        page!
+      </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Color.js
@@ -0,0 +1,9 @@
+export default function Color ({ hex, name }) {
+  return (
+    <div
+      className='color-square' 
+    >
+      <h2>{name}</h2>
+    </div>
+  )
+}
